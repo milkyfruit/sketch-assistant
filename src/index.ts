63638a27f982298fa296583a8f24850ec01eefd7
Domas Markevičius <domasm@wix.com>
2020-09-08 15:50:44 +0300

    Code that checks the applied color
    If the color is not red, then assistant prints a warning.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
-import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+import {
+  AssistantPackage,
+  RuleDefinition,
+  FileFormat
+} from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const styleHasDisabledFill = (style: FileFormat.Style): boolean =>
+  Array.isArray(style.fills) && style.fills.some((fill) => fill.isEnabled)
+
+const wsrColors: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    const { utils } = context
+
+
+    for (const layer of utils.objects.anyLayer) {
+      if (!('style' in layer)) continue // Narrow type to layers with a `style` prop
+      if (!layer.style) continue // Narrow type to truthy `style` prop
+      if (typeof layer.sharedStyleID === 'string') continue // Ignore layers using a shared style
+
+
+      if (styleHasDisabledFill(layer.style)) {
+        const red = layer.style.fills?.[0]?.color?.red
+        const green = layer.style.fills?.[0]?.color?.green
+        const blue = layer.style.fills?.[0]?.color?.blue
+
+        if (!(red == 1 && green == 0 && blue == 0)) {
+          utils.report(`Layer “${layer.name}” is NOT red`, layer)
+        }
+      }
+    }
   },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'sketch-assistant-template/wsr-colors',
+  title: 'This color is NOT red',
+  description: 'asd',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
     name: 'sketch-assistant-template',
-    rules: [helloWorld],
+    rules: [wsrColors],
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'sketch-assistant-template/wsr-colors': {
+          active: true
+        },
       },
     },
   }
